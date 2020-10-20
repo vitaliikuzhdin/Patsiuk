65f7910ae4edbb90669fad64f2072dbe1937b75b
vitaliy172 <68148475+vitaliy172@users.noreply.github.com>
2020-10-20 14:25:00 +0300

    Update BTsketch.ino

--- BTsketch/BTsketch.ino
+++ BTsketch/BTsketch.ino
@@ -6,7 +6,7 @@
 SoftwareSerial BTserial(rx, tx);//rx, tx
 
 char current_command;
-String commands;
+String strCommands;
 bool BTstop = false;
 
 void setup() {
@@ -18,10 +18,11 @@ void loop() {
   while (BTstop == false){
     while (BTserial.available() > 0){
       current_command = BTserial.read();
-      if (current_command != 's')
-        commands += current_command;
-      else
+      if (current_command != 's'){
+        commands += current_command;}
+      else{
         BTstop = true;
+        commands += current_command;}
     }
   }
   Serial.println(commands);//only for tests
