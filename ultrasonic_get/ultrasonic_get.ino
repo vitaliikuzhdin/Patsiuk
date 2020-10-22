e3681a1cc36747b9849efff4d97adb1339eef351
vitaliy172 <68148475+vitaliy172@users.noreply.github.com>
2020-10-22 09:30:59 +0300

    Update ultrasonic_get.ino

--- ultrasonic_get/ultrasonic_get.ino
+++ ultrasonic_get/ultrasonic_get.ino
@@ -10,9 +10,6 @@
 
 #define MAX_DISTANCE 200
 
-int leftSonarSumm;
-int rightSoarSumm
-
 NewPing RIGHT_SONAR(RIGHT_TRIG, RIGHT_ECHO, MAX_DISTANCE);
 NewPing LEFT_SONAR(LEFT_TRIG, LEFT_ECHO, MAX_DISTANCE);
 
@@ -20,8 +17,8 @@ void setup() {
   pinMode(LEFT_SONAR_VCC, OUTPUT);
   pinMode(RIGHT_SONAR_VCC, OUTPUT);
   
-  digitalWrite(RIGHT_SONAR_VCC, HIGH);//питание правому ультразвуку
-  digitalWrite(LEFT_SONAR_VCC, HIGH);//питание левому ультразвуку
+  digitalWrite(RIGHT_SONAR_VCC, HIGH);//power for right sonar
+  digitalWrite(LEFT_SONAR_VCC, HIGH);//power for left sonar
   
   Serial.begin(9600);
   while(!Serial){};
@@ -32,17 +29,17 @@ void loop() {
   
 }
 byte getLeftUS(){
-  leftSonarSumm = 0;
-  for(byte i = 0; i < 10; i++){
+  int leftSonarSumm = 0;
+  for(byte i = 0; i < 20; i++){
     leftSonarSumm += LEFT_SONAR.ping_cm(); 
   }
-  return leftSonarSumm / 10;
+  return leftSonarSumm / 20;
 }
 
 byte getRightUS(){
   righttSonarSumm = 0;
-  for(byte i = 0; i < 10; i++){
+  for(byte i = 0; i < 20; i++){
     rightSonarSumm += LEFT_SONAR.ping_cm(); 
   }
-  return righttSonarSumm / 10;
+  return righttSonarSumm / 20;
 }
