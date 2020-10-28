2dc92046524af1c3011f04384a525124c202eae1
vitaliy172 <68148475+vitaliy172@users.noreply.github.com>
2020-10-28 12:04:31 +0200

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -52,218 +52,218 @@ int xTravel;
 int yTravel;
 
 void setup(){
-  pinMode(metal_input, INPUT);
-  
-  pinMode(RIGHT_FRONT_D, OUTPUT);
-  pinMode(RIGHT_FRONT_PWM, OUTPUT);
-   
-  pinMode(RIGHT_BACK_D, OUTPUT);
-  pinMode(RIGHT_BACK_PWM, OUTPUT);
+    pinMode(metal_input, INPUT);
+    
+    pinMode(RIGHT_FRONT_D, OUTPUT);
+    pinMode(RIGHT_FRONT_PWM, OUTPUT);
+     
+    pinMode(RIGHT_BACK_D, OUTPUT);
+    pinMode(RIGHT_BACK_PWM, OUTPUT);
 
-  pinMode(LEFT_FRONT_D, OUTPUT);
-  pinMode(LEFT_FRONT_PWM, OUTPUT);
+    pinMode(LEFT_FRONT_D, OUTPUT);
+    pinMode(LEFT_FRONT_PWM, OUTPUT);
 
-  pinMode(LEFT_BACK_D, OUTPUT);
-  pinMode(LEFT_BACK_PWM, OUTPUT);
-  
-  pinMode(RIGHT_SONAR_VCC, OUTPUT);
-  pinMode(LEFT_SONAR_VCC, OUTPUT);
+    pinMode(LEFT_BACK_D, OUTPUT);
+    pinMode(LEFT_BACK_PWM, OUTPUT);
+    
+    pinMode(RIGHT_SONAR_VCC, OUTPUT);
+    pinMode(LEFT_SONAR_VCC, OUTPUT);
 
-  digitalWrite(RIGHT_SONAR_VCC, HIGH);//power for right sonar
-  digitalWrite(LEFT_SONAR_VCC, HIGH);//power for left sonar
+    digitalWrite(RIGHT_SONAR_VCC, HIGH);//power for right sonar
+    digitalWrite(LEFT_SONAR_VCC, HIGH);//power for left sonar
 
-  RIGHT_FRONT.setResolution(8);
-  RIGHT_BACK.setResolution(8);
-  LEFT_FRONT.setResolution(8);
-  LEFT_BACK.setResolution(8);
+    RIGHT_FRONT.setResolution(8);
+    RIGHT_BACK.setResolution(8);
+    LEFT_FRONT.setResolution(8);
+    LEFT_BACK.setResolution(8);
 
-  RIGHT_FRONT.setDirection(NORMAL);
-  RIGHT_BACK.setDirection(NORMAL);
-  LEFT_FRONT.setDirection(REVERSE);
-  LEFT_BACK.setDirection(NORMAL);
+    RIGHT_FRONT.setDirection(NORMAL);
+    RIGHT_BACK.setDirection(NORMAL);
+    LEFT_FRONT.setDirection(REVERSE);
+    LEFT_BACK.setDirection(NORMAL);
 
-  RIGHT_FRONT.setMinDuty(minDuty);
-  RIGHT_BACK.setMinDuty(minDuty);
-  LEFT_FRONT.setMinDuty(minDuty);
-  LEFT_BACK.setMinDuty(minDuty);
-  
-  RIGHT_FRONT.setMode(AUTO);
-  RIGHT_BACK.setMode(AUTO);
-  LEFT_FRONT.setMode(AUTO);
-  LEFT_BACK.setMode(AUTO);
-  
-  Serial.begin(9600);//only for tests
-  BTserial.begin(9600); 
+    RIGHT_FRONT.setMinDuty(minDuty);
+    RIGHT_BACK.setMinDuty(minDuty);
+    LEFT_FRONT.setMinDuty(minDuty);
+    LEFT_BACK.setMinDuty(minDuty);
+    
+    RIGHT_FRONT.setMode(AUTO);
+    RIGHT_BACK.setMode(AUTO);
+    LEFT_FRONT.setMode(AUTO);
+    LEFT_BACK.setMode(AUTO);
+    
+    Serial.begin(9600);//only for tests
+    BTserial.begin(9600); 
 }
 
-void loop(){
-  read_commands();
-  bool finished_ride = false;
-  while (finished_ride == false){
-    if (getRightUS() > 20 and getLeftUS() > 20){
-      if (currentCommandChar() == 'f'){
-        forward();
-      }
-      else if (currentCommandChar() == 'b'){
-        back();
-      }
-      else if (currentCommandChar() == 'r'){
-        right();
-      }
-      else if (currentCommandChar() == 'l'){
-        left();
-      }
-      else{//currentCommandChar() == 's'
-      finished_ride = true;  
-      }
+void loop() {
+    read_commands();
+    bool finished_ride = false;
+    while (finished_ride == false){
+        if (getRightUS() > 20 and getLeftUS() > 20){
+            if (currentCommandChar() == 'f'){
+                forward();
+            }
+            else if (currentCommandChar() == 'b'){
+                back();
+            }
+            else if (currentCommandChar() == 'r'){
+                right();
+            }
+            else if (currentCommandChar() == 'l'){
+                left();
+            }
+            else{//currentCommandChar() == 's'
+            finished_ride = true;    
+            }
+        }
+        else{
+            //avoid obstacles
+            right();
+            forward();
+            left(); 
+        }
+    }
+    //return home
+    if (yTravel > 0){
+        while (angle != 180){
+            right();    
+        }
+        for (byte i = 0; i < yTravel; i++){
+            forward();    
+        }
     }
     else{
-      //if getRightUS and getLeftUS < 20
-      right();
-      forward();
-      left(); 
+        while (angle != 0){
+            right();    
+        }
+        for (byte i = 0; i < yTravel; i++){
+            forward();    
+        }
     }
-  }
-  //return home
-  if (yTravel > 0){
-    while (angle != 180){
-      right();  
-    }
-    for (byte i = 0; i < yTravel; i++){
-      forward();  
-    }
-  }
-  else{
-    while (angle != 0){
-      right();  
-    }
-    for (byte i = 0; i < yTravel; i++){
-      forward();  
-    }
-  }
 }
 
 char currentCommandChar(){
-  return strCommands.charAt(commandSerialNum++);
+    return strCommands.charAt(commandSerialNum++);
 }
 
 byte getLeftUS(){
-  int leftSonarSumm = 0;
-  for (byte i = 0; i < 20; i++){
-    leftSonarSumm += LEFT_SONAR.ping_cm(); 
-  }
-  return leftSonarSumm / 20;
+    int leftSonarSumm = 0;
+    for(byte i = 0; i < 20; i++){
+        leftSonarSumm += LEFT_SONAR.ping_cm(); 
+    }
+    return leftSonarSumm / 20;
 }
 
 byte getRightUS(){
-  int rightSonarSumm = 0;
-  for (byte i = 0; i < 20; i++){
-    rightSonarSumm += RIGHT_SONAR.ping_cm(); 
-  }
-  return rightSonarSumm / 20;
+    int rightSonarSumm = 0;
+    for(byte i = 0; i < 20; i++){
+        rightSonarSumm += RIGHT_SONAR.ping_cm(); 
+    }
+    return rightSonarSumm / 20;
 }
 
 void read_commands(){
-  bool BTstop = false;
-  while (BTstop == false){
-    while (BTserial.available() > 0){
-      char current_command = BTserial.read();
-      if (current_command == 'c'){
-        strCommands = "";
-      }
-      else if (current_command != 's'){
-        for(byte i = 0; i < 10; i++){//because 1 command = 10 cm
-          strCommands += current_command;
+    bool BTstop = false;
+    while (BTstop == false){
+        while (BTserial.available() > 0){
+            char current_command = BTserial.read();
+            if (current_command == 'c'){
+                strCommands = "";
+            }
+            else if (current_command != 's'){
+                for(byte i = 0; i < 10; i++){//because 1 command = 10 cm
+                    strCommands += current_command;
+                }
+            }
+            else{
+                strCommands += current_command;
+                BTstop = true;
+            }
         }
-      }
-      else{
-        strCommands += current_command;
-        BTstop = true;
-      }
     }
-  }
 }
 
 void right(){
-  RIGHT_FRONT.smoothTick(-255);
-  RIGHT_BACK.smoothTick(-255);
-  LEFT_FRONT.smoothTick(255);
-  LEFT_BACK.smoothTick(255);
+    RIGHT_FRONT.smoothTick(-255);
+    RIGHT_BACK.smoothTick(-255);
+    LEFT_FRONT.smoothTick(255);
+    LEFT_BACK.smoothTick(255);
 
-  BTserial.print('r');
+    BTserial.print('r');
 
-  angle += 90;
-  if (angle == 360){
-    angle = 0;  
-  }
-  delay(timeForTurning);
+    angle += 90;
+    if (angle == 360){
+        angle = 0;    
+    }
+    delay(timeForTurning);
 }
 
 void left(){
-  RIGHT_FRONT.smoothTick(255);
-  RIGHT_BACK.smoothTick(255);
-  LEFT_FRONT.smoothTick(-255);
-  LEFT_BACK.smoothTick(-255);
+    RIGHT_FRONT.smoothTick(255);
+    RIGHT_BACK.smoothTick(255);
+    LEFT_FRONT.smoothTick(-255);
+    LEFT_BACK.smoothTick(-255);
 
-  BTserial.print('l');
+    BTserial.print('l');
 
-  angle -= 90;
-  if (angle == 360){
-    angle = 0;  
-  }
-  delay(timeForTurning);
+    angle -= 90;
+    if (angle == 360){
+        angle = 0;    
+    }
+    delay(timeForTurning);
 }
 
 void forward(){
-  RIGHT_FRONT.smoothTick(255);
-  RIGHT_BACK.smoothTick(255);
-  LEFT_FRONT.smoothTick(255);
-  LEFT_BACK.smoothTick(255);
-  
-  BTserial.print('f');
+    RIGHT_FRONT.smoothTick(255);
+    RIGHT_BACK.smoothTick(255);
+    LEFT_FRONT.smoothTick(255);
+    LEFT_BACK.smoothTick(255);
+    
+    BTserial.print('f');
 
-  if (angle == 0){
-   yTravel++;  
-  }
-  else if (angle == 90){
-    xTravel++;  
-  }
-  else if (angle == 180){
-    yTravel--;  
-  }
-  else{//angle == 270
-     xTravel--; 
-  }
-  for (int i = 0; i < timeForRiding; i++){
-    BTserial.print(analogRead(metal_input));
-    BTserial.flush();
-    delay(1);
-  }
+    if (angle == 0){
+     yTravel++;    
+    }
+    else if (angle == 90){
+        xTravel++;    
+    }
+    else if (angle == 180){
+        yTravel--;    
+    }
+    else{//angle == 270
+         xTravel--; 
+    }
+    for (int i = 0; i < timeForRiding; i++){
+        BTserial.print(analogRead(metal_input));
+        BTserial.flush();
+        delay(1);
+    }
 }
 
 void back(){
-  RIGHT_FRONT.smoothTick(-255);
-  RIGHT_BACK.smoothTick(-255);
-  LEFT_FRONT.smoothTick(-255);
-  LEFT_BACK.smoothTick(-255);
+    RIGHT_FRONT.smoothTick(-255);
+    RIGHT_BACK.smoothTick(-255);
+    LEFT_FRONT.smoothTick(-255);
+    LEFT_BACK.smoothTick(-255);
 
-  BTserial.print('b');
+    BTserial.print('b');
 
-  if (angle == 0){
-   yTravel--;  
-  }
-  else if (angle == 90){
-    xTravel--;  
-  }
-  else if (angle == 180){
-    yTravel++;  
-  }
-  else{//angle == 270
-     xTravel++; 
-  }
-  for (int i = 0; i < timeForRiding; i++){
-    BTserial.print(analogRead(metal_input));
-    BTserial.flush();
-    delay(1);
-  }
+    if (angle == 0){
+        yTravel--;    
+    }
+    else if (angle == 90){
+        xTravel--;    
+    }
+    else if (angle == 180){
+        yTravel++;    
+    }
+    else{//angle == 270
+         xTravel++; 
+    }
+    for (int i = 0; i < timeForRiding; i++){
+        BTserial.print(analogRead(metal_input));
+        BTserial.flush();
+        delay(1);
+    }
 }
